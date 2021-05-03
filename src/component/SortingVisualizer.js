08a76a21b4fc9a930726105f839899b52f9ac535
ItoKobi <ito.kobilol@gmail.com>
2021-05-03 10:35:14 -0400

    Done

--- src/component/SortingVisualizer.js
+++ src/component/SortingVisualizer.js
@@ -5,8 +5,9 @@ function SortingVisualizer() {
     const [values, setValues] = useState([]);
     const [timesRan, setTimesRan] = useState(0);
     const [swaps, setSwaps] = useState(0);
-    const [size, setSize] = useState(100);
+    const [size, setSize] = useState(Number(window.innerWidth) / 10 - 5);
     const [speed, setSpeed] = useState(100);
+    const [animations, setAnimations] = useState(true);
 
     const generateValues = (rSize = size) => {
         let output = [];
@@ -109,42 +110,87 @@ function SortingVisualizer() {
         }
     }
 
-    const heapify = (arr, n, i) => {
-        let largest = i;
-        let l = 2 * i + 1;
-        let r = 2 * i + 2;
+const sortAlgorithmHeapSort = () => {
+    let arr = [...values]
+    let times_ran = 0;
+    let swaps_ran = 0;
 
-        if(l < n && arr[largest].value < arr[l].value){
-            largest = l;
-        }
-        if(r < n && arr[largest].value < arr[r].value){
-            largest = r;
-        }
-        if(largest !== i){
-            var temp = arr[i]
-            arr[i] = arr[largest];
-            arr[largest] = temp;
-            heapify(arr, n, largest)
+    const buildMaxHeap = (arr) => {
+        let i = Math.floor(arr.length / 2 - 1);
+        while (i >= 0) {
+            heapify(arr, i, arr.length);
+            i -= 1;
+            times_ran++
+            setTimesRan(times_ran)
         }
     }
 
-    const heapSort = () => {
-        let arr = [...values];
-        let n = arr.length;
-
-        for(var i = n / 2 -1; i >= 0; i-- ){
-            heapify(arr, n, i);
+    const heapify = (heap, i, max) => {
+        let index;
+        let leftChild;
+        let rightChild;
+        while (i < max) {
+            index = i;
+            leftChild = 2 * i + 1;
+            rightChild = leftChild + 1;
+            if (leftChild < max && heap[leftChild].value > heap[index].value) {
+                index = leftChild;
+            }
+            if (rightChild < max && heap[rightChild].value > heap[index].value) {
+                index = rightChild;
+            }
+            if (index === i) {
+                return;
+            }
+            times_ran++
+            setTimesRan(times_ran)            
+            heap[index].working = true;
+            swap(heap, i, index);
+            heap[index].working = false;
+            i = index;
         }
-
-        for(var j = n - 1; i > 0; j--){
-            var temp = arr[0];
-            arr[0] = arr[j];
-            arr[j] = temp;
-
-            heapify(arr, j, 0);
-        }
-
     }
+
+    const swap = async (arr, firstItemIndex, lastItemIndex) => {
+        const temp = arr[firstItemIndex].value;
+        arr[firstItemIndex].value = arr[lastItemIndex].value;
+        arr[lastItemIndex].value = temp;
+        arr[firstItemIndex].working = true;
+        arr[lastItemIndex].working = true;
+        await new Promise(r => setTimeout(r, speed)).then(() => {
+            setValues([...arr])
+        })
+        arr[firstItemIndex].working = false;
+        arr[lastItemIndex].working = false;        
+        times_ran++
+        setTimesRan(times_ran)
+        swaps_ran++
+        setSwaps(swaps_ran)
+    }
+
+    const heapSort = async (arr) => {    
+    buildMaxHeap(arr);
+    await new Promise(r => setTimeout(r, speed)).then(() => {
+        setValues([...arr])
+    })
+    let lastElement = arr.length - 1;
+    while (lastElement > 0) {
+        swap(arr, 0, lastElement);
+        heapify(arr, 0, lastElement);
+        lastElement -= 1;
+        times_ran++
+        setTimesRan(times_ran)        
+        await new Promise(r => setTimeout(r, speed)).then(() => {
+            setValues([...arr])
+        })
+        }    
+    }
+    heapSort(arr);
+} 
+
+const handleAnimations = () => {
+    setAnimations(!animations)
+}
 
 
     useEffect(() => {
@@ -154,33 +200,40 @@ function SortingVisualizer() {
 
     return (
         <div>
-            <nav className="bg-gray-500 p-6 flex justify-between">
-                <div>
-                    <button className="bg-blue-400 px-4 py-2 rounded-lg shadow mx-1" onClick={() => sortAlgorithmBubbleSort()}>Bubble Sort</button>
-                    <button className="bg-blue-400 px-4 py-2 rounded-lg shadow mx-1" onClick={() => sortAlgorithmInsertionSort()}>Insertion Sort</button>
-                    <button className="bg-blue-400 px-4 py-2 rounded-lg shadow mx-1" onClick={() => sortAlgorithmSelectionSort()}>Selection Sort</button>
-                    <button className="bg-blue-400 px-4 py-2 rounded-lg shadow mx-1" onClick={() => console.log(heapSort())}>Heap Sort</button>
-                    <button className="bg-blue-400 px-4 py-2 rounded-lg shadow mx-1" onClick={() => generateValues()}>New Values</button>
+            <div className="bg-gray-600 w-full h-6 flex justify-center items-center text-white font-bold">
+                <a href="https://github.com/kobito-kun/Sorting-Visualizer"><i className="fab fa-github"></i> Kobi at Github</a>
+            </div>
+            <nav className="bg-gray-500 p-6 flex flex-col lg:flex-row lg:justify-between">
+                <div className="flex flex-col lg:flex-row items-center justify-center">
+                    <button className="font-bold text-gray-600 shadow bg-blue-400 lg:w-auto w-full px-4 py-2 rounded-lg shadow m-1" onClick={() => sortAlgorithmBubbleSort()}>Bubble Sort</button>
+                    <button className="font-bold text-gray-600 shadow bg-blue-400 lg:w-auto w-full px-4 py-2 rounded-lg shadow m-1" onClick={() => sortAlgorithmInsertionSort()}>Insertion Sort</button>
+                    <button className="font-bold text-gray-600 shadow bg-blue-400 lg:w-auto w-full px-4 py-2 rounded-lg shadow m-1" onClick={() => sortAlgorithmSelectionSort()}>Selection Sort</button>
+                    <button className="font-bold text-gray-600 shadow bg-blue-400 lg:w-auto w-full px-4 py-2 rounded-lg shadow m-1" onClick={() => sortAlgorithmHeapSort()}>Heap Sort</button>
+                    <button className="font-bold text-gray-600 shadow bg-blue-400 lg:w-auto w-full px-4 py-2 rounded-lg shadow m-1" onClick={() => generateValues()}>New Values</button>
                 </div>
-                <div className="flex">
+                <div className="flex justify-center items-center">
+                    <div className="flex flex-col mx-1">
+                        <label for="animations">Animations?</label>
+                        <input type="checkbox" id="animations" className="block mx-auto" onChange={handleAnimations} checked={animations} />
+                    </div>
                     <div className="flex flex-col mx-1">
                     <label for="size">Set Size {size}</label>
-                    <input onChange={updateSize} id="size" type="range" min="10" step="5" max="300"></input>
+                    <input onChange={updateSize} id="size" type="range" min="10" step="5" max={Number(window.innerWidth) / 10 - 5}></input>
                     </div>
                     <div className="flex flex-col mx-1">
                     <label for="speed">Set Speed {speed}ms</label>
                     <input onChange={updateSpeed} id="speed" type="range" min="0" step="5" max="300"></input>
                     </div>
                 </div>
-                <div className="px-4">
+                <div className="px-4 lg:block flex justify-between">
                     <h3 className="font-bold">Loops ran: {timesRan}</h3>
                     <h3 className="font-bold">Swaps: {swaps}</h3>
                 </div>
             </nav>
-            <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center" style={{overflowX: 'hidden'}}>
                 <div className="flex">
                 {values.map((value, idx) => (
-                    <div key={idx} style={{height: `${value.value / 2}px`, width: "10px"}} className={`${value.working ? 'bg-red-500' : 'bg-blue-500'} hover:bg-blue-600 hover:shadow`}></div>
+                    <div key={idx} style={{height: `${value.value / 2}px`, width: "10px"}} className={`${value.working ? 'bg-red-500' : 'bg-blue-500'} ${animations ? 'duration-200' : ''} hover:bg-blue-600 hover:shadow`}></div>
                     ))}
                 </div>
             </div>
